62b395280d0200b8575de37bc8259cc6634ff658
Jason Stine <jason102@gmail.com>
2024-03-26 22:42:18 -0700

    feat: basic MUI foudation code and page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,80 @@
 import { useState } from "react";
 
-import "./App.css";
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import FormControl from "@mui/material/FormControl";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Radio from "@mui/material/Radio";
+import Grid from "@mui/material/Grid";
+import Pagination from "@mui/material/Pagination";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [images, setImages] = useState([]);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container component="main" sx={{ mb: 4 }}>
+      <Paper
+        variant="elevation"
+        elevation={16}
+        sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+      >
+        <h1>Unsplash Photo Browser</h1>
+        <Box sx={{ display: "flex", flexDirection: "row" }}>
+          <Box sx={{ display: "flex", flex: 1 }}>
+            <TextField
+              required
+              id="searchField"
+              name="searchField"
+              label="Search photos..."
+              fullWidth
+              variant="standard"
+            />
+          </Box>
+          <Box>
+            <FormControl>
+              <RadioGroup
+                aria-labelledby="filter-radio-buttons-group-label"
+                defaultValue="relevant"
+                name="filter-radio-buttons-group"
+                row
+              >
+                <FormControlLabel
+                  value="relevant"
+                  control={<Radio />}
+                  label="Relevant"
+                />
+                <FormControlLabel
+                  value="latest"
+                  control={<Radio />}
+                  label="Latest"
+                />
+              </RadioGroup>
+            </FormControl>
+          </Box>
+        </Box>
+        <Grid container spacing={2} sx={{ height: 500 }}>
+          {images.map((image, index) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+              <Typography>image placeholder</Typography>
+            </Grid>
+          ))}
+        </Grid>
+        <Pagination
+          count={1} // Set the total count of pages
+          page={1}
+          color="primary"
+          style={{
+            marginTop: "20px",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        />
+      </Paper>
+    </Container>
   );
 }
 
