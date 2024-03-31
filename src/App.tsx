415e053a43beddc29497e5ca9d898900befeccbb
Jason Stine <jason102@gmail.com>
2024-03-31 11:33:49 -0700

    refactor: create pages directory and move App code into its own Home page folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
 import React from "react";
 
-import Container from "@mui/material/Container";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-
-import SearchControls from "src/SearchControls";
-import ImageSearchResults from "src/ImageSearchResults";
-import SearchFiltersContext from "src/context/SearchFiltersContext";
+import Home from "src/pages/Home";
 
 const App: React.FC = () => {
-  return (
-    <SearchFiltersContext>
-      <Container component="main" sx={{ mb: 4 }}>
-        <Paper
-          variant="elevation"
-          elevation={16}
-          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
-        >
-          <Typography variant="h5" textAlign="center">
-            Unsplash Photo Browser
-          </Typography>
-          <SearchControls />
-          <ImageSearchResults />
-        </Paper>
-      </Container>
-    </SearchFiltersContext>
-  );
+  return <Home />;
 };
 
 export default App;
